26b3072507783b5b6b57be1716dc16b744b9f247
Andreluis-Ingaroca <67808076+Andreluis-Ingaroca@users.noreply.github.com>
2022-11-19 23:32:43 -0500

    Update Update.vue

--- src/views/Update.vue
+++ src/views/Update.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="pt-5 pl-5 pr-5">
       <v-card color="#55B4B0" rounded="xl">
-        <v-card-title class="justify-center text-h3 pt-5 pb-5 font-weight-black">UPDATE QUANTITY</v-card-title>
+        <v-card-title class="justify-center text-h4 pt-5 pb-5 font-weight-black">ACTUALIZAR POR EL ID DE LA MEDICINA</v-card-title>
       </v-card>
 
     <v-row class="pt-10"> 
         <v-col cols="12" sm="8">
                 <v-card color="#55B4B0" rounded="xl">
-                    <v-card-title class="justify-center text-h5 pt-5 pb-5 font-weight-black">ENTER MEDICINE ID</v-card-title>
+                    <v-card-title class="justify-center text-h5 pt-5 pb-5 font-weight-black">INGRESAR ID DE LA MEDICINA</v-card-title>
 
                     <form @submit.prevent="submit" color="blue lighten-1">
-                    <v-select :items="ids" label="ID MEDICINE" class="pl-10 pt-2 pr-10" v-model="selectId" color="white"></v-select>
+                    <v-select :items="ids" label="ID MEDICINA" class="pl-10 pt-2 pr-10" v-model="selectId" color="white"></v-select>
                     <v-col cols="12" class="pb-3 pl-10 pt-2 pr-10">
-                        <v-btn @click="submit(selectId)" block color="blue lighten-1">SUBMIT</v-btn>
+                        <v-btn @click="submit(selectId)" block color="blue lighten-1">ENVIAR</v-btn>
                     </v-col>
                     </form>
                 </v-card>
                 <h1 class="pt-10"></h1>
                 <v-card v-if="hide == false" color="#55B4B0" rounded="xl">
-                    <v-card-title class="justify-center text-h5 pt-5 pb-5 font-weight-black">SELECTED MEDICINE</v-card-title>
+                    <v-card-title class="justify-center text-h5 pt-5 pb-5 font-weight-black">MEDICINA SELECCIONADA</v-card-title>
                     <form @submit.prevent="update" color="blue lighten-1">
                     <vue-title title="Static Title"></vue-title>
-                    <v-text-field color="white" v-model="name" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: name}" label="Generic Name"></v-text-field>
-                    <v-text-field color="white" v-model="concentration" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: concentration }" label="Concentration"></v-text-field>
-                    <v-text-field color="white" v-model="form" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: form }" label="Pharmaceutical Form"></v-text-field>
-                    <v-text-field color="white" type="date" v-model="expiration" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: expiration }" label="Expiration Date"></v-text-field>
-                    <v-text-field color="white" type="number" v-model="quantity" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: quantity }" label="Quantity"></v-text-field>
-                    <v-text-field color="white" class="pl-10 pt-2 pr-10" v-model="barcode" v-bind="{ placeholder: barcode }" label="Barcode - Lot" disabled></v-text-field>
+                    <v-text-field color="white" v-model="name" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: name}" label="Nombre Génerico"></v-text-field>
+                    <v-text-field color="white" v-model="concentration" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: concentration }" label="Concentración"></v-text-field>
+                    <v-text-field color="white" v-model="form" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: form }" label="Forma Farmacéutica"></v-text-field>
+                    <v-text-field color="white" type="date" v-model="expiration" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: expiration }" label="Fecha de Expiración"></v-text-field>
+                    <v-text-field color="white" type="number" v-model="quantity" class="pl-10 pt-2 pr-10" v-bind="{ placeholder: quantity }" label="Cantidad"></v-text-field>
+                    <v-text-field color="white" class="pl-10 pt-2 pr-10" v-model="barcode" v-bind="{ placeholder: barcode }" label="Lote - Código" disabled></v-text-field>
                     <v-col cols="12" class="pb-3 pl-10 pt-2 pr-10">
-                        <v-btn @click="update" block color="blue lighten-1">UPDATE</v-btn>
+                        <v-btn @click="update" block color="blue lighten-1">ACTUALIZAR</v-btn>
                     </v-col>
                     </form>
                 </v-card>
@@ -43,7 +43,7 @@
       <v-snackbar v-model="snackbar" :timeout="-1" :value="true" bottom :multi-line="multiLine" color="primary">{{ text }}
         <template v-slot:action="{ attrs }">
           <v-btn color="red" text v-bind="attrs" @click="snackbar = false">
-            Close
+            Cerrar
           </v-btn>
         </template>
       </v-snackbar> 
@@ -89,7 +89,7 @@ export default {
             } else {
                 this.snb = true;
                 this.snackbar = true;
-                this.text = 'NO ID SELECTED';
+                this.text = 'NO EXISTE ID SELECCIONADO';
                 console.log('NO ID SELECTED')
             }
         
